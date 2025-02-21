e49edd090866ddafb09360c4f49d44a980322af8
Hajavololona Armellin <armelgeek5@gmail.com>
2025-02-21 12:33:22 +0300

    feat: generic list

--- core/application/services/brand/brand.service.ts
+++ core/application/services/brand/brand.service.ts
@@ -1,42 +1,48 @@
 import { BrandRepositoryImpl, type BrandRepository } from '@/core/application/repository/brand.repository';
-import type { Brand, BrandPayload, PaginatedBrand } from '@/core/domain/types/brand.type';
+import type { Brand, BrandPayload } from '@/core/domain/types/brand.type';
 import type { Filter } from '@/shared/lib/types/filter';
+import { BaseService, PaginatedResponse } from '@/core/application/services/base.service';
 
 export interface BrandService {
-  list(filter: Filter): Promise<PaginatedBrand>;
+  list(filter: Filter): Promise<PaginatedResponse<Brand>>;
   detail(slug: string): Promise<Brand>;
   create(payload: BrandPayload): Promise<Brand>;
-  update(slug: string, payload: BrandPayload): Promise<{ message: string }>;
+  update(slug: string, payload: Partial<Brand>): Promise<Brand>;
   delete(slug: string): Promise<{ message: string }>;
+  findById(id: string): Promise<Brand>;
 }
 
-export class BrandServiceImpl implements BrandService {
+export class BrandServiceImpl extends BaseService<Brand, { message: string }> implements BrandService {
   private readonly brandRepository: BrandRepository;
 
   constructor(
     brandRepository: BrandRepository = new BrandRepositoryImpl()
   ) {
+    super();
     this.brandRepository = brandRepository;
   }
 
-  async list(filter: Filter): Promise<PaginatedBrand> {
-    return await this.brandRepository.list(filter);
+  async list(filter: Filter): Promise<PaginatedResponse<Brand>> {
+    return this.brandRepository.list(filter);
   }
 
   async detail(slug: string): Promise<Brand> {
-    return await this.brandRepository.detail(slug);
+    return this.brandRepository.detail(slug);
   }
 
   async create(payload: BrandPayload): Promise<Brand> {
-
-    return this.brandRepository.create({ ...payload });
+    return this.brandRepository.create(payload);
   }
 
-  async update(slug: string, payload: BrandPayload): Promise<{ message: string }> {
-    return await this.brandRepository.update(slug, { ...payload });
+  async update(slug: string, payload: Partial<Brand>): Promise<Brand> {
+    return this.brandRepository.update(slug, payload);
   }
 
   async delete(slug: string): Promise<{ message: string }> {
-    return await this.brandRepository.delete(slug);
+    return this.brandRepository.delete(slug);
+  }
+
+  async findById(id: string): Promise<Brand> {
+    return this.brandRepository.findById(id);
   }
 }
